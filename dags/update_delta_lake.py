7e0b89821ecc705229864efb3ef971a61772ec16
zaracky <51820857+zaracky@users.noreply.github.com>
2025-06-18 23:30:59 +0200

    Version ok

--- dags/update_delta_lake.py
+++ dags/update_delta_lake.py
@@ -1,28 +1,50 @@
+from datetime import datetime
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
-from datetime import datetime
 from docker.types import Mount
 
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 0,
+}
+
 with DAG(
-    dag_id="update_delta_lake",
-    start_date=datetime(2024, 1, 1),
-    catchup=False,
+    dag_id='update_delta_lake',
+    default_args=default_args,
     schedule_interval=None,
-    is_paused_upon_creation=False,
+    catchup=False,
+    description='Ingestion depuis PostgreSQL, puis export CSV Power BI',
 ) as dag:
 
-    spark_job = DockerOperator(
-        task_id="run_spark_job",
-        image="custom-spark:delta",
-        container_name="spark-job",
-        api_version="auto",
+    run_spark_job = DockerOperator(
+        task_id='run_spark_job',
+        image='custom-spark:delta',
+        api_version='auto',
         auto_remove=True,
-        command="spark-submit /opt/spark-apps/spark_write_delta.py",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="container:p12-airflow-webserver-1",
-        mounts=[
-            Mount(source="C:/Users/Loic/Documents/p12/spark-jobs", target="/opt/spark-apps", type="bind"),
-            Mount(source="C:/Users/Loic/Documents/p12/data", target="/opt/spark-data", type="bind"),
-        ],
+        command='spark-submit /opt/spark-apps/spark_write_delta.py',
+        docker_url='unix://var/run/docker.sock',
+        network_mode='p12_airflow_net',  
         mount_tmp_dir=False,
+        mounts=[
+            Mount(source='/host_mnt/c/Users/Loic/Documents/p12/spark-jobs', target='/opt/spark-apps', type='bind'),
+            Mount(source='/host_mnt/c/Users/Loic/Documents/p12/spark-data', target='/opt/spark-data', type='bind'),
+        ],
     )
+
+    export_csv = DockerOperator(
+        task_id='export_to_csv',
+        image='custom-spark:delta',
+        api_version='auto',
+        auto_remove=True,
+        command='spark-submit /opt/spark-apps/spark_export_csv.py',
+        docker_url='unix://var/run/docker.sock',
+        network_mode='p12_airflow_net',  
+        mount_tmp_dir=False,
+        mounts=[
+            Mount(source='/host_mnt/c/Users/Loic/Documents/p12/spark-jobs', target='/opt/spark-apps', type='bind'),
+            Mount(source='/host_mnt/c/Users/Loic/Documents/p12/spark-data', target='/opt/spark-data', type='bind'),
+        ],
+    )
+
+    run_spark_job >> export_csv
